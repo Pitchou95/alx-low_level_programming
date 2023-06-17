cf3fde279c5ba05512b0d63a2d81205a21573ded
pitchou95 <pitchoudeve@gmail.com>
2023-06-17 10:14:43 -0700

    Edit task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,12 +12,14 @@
  *
  */
 
-int main (void)
+int main(void)
 
 {
      int n;
-     srand (time(0));
+
+     srand(time(0));
      n = rand() - RAND_MAX / 2;
+
      if (n > 0)
      {      
      printf("%d is positive\n", n);
@@ -28,7 +30,7 @@ int main (void)
      }
      else
      {
-     printf1-last_digit.c("%d is negative\n", n);
+     printf("%d is negative\n", n);
      }
      return (0);
 }
