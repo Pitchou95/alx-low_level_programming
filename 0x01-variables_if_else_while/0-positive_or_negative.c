26b732b1abd11517db698ca0141be0224e80ec85
pitchou95 <pitchoudeve@gmail.com>
2023-06-19 10:53:18 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,11 @@
 #include <stdlib.h>
-
 #include <time.h>
-
 #include <stdio.h>
 
-
-
 /**
- * main - Prints if number is positive, zero or negative
- * Return: Always (Success)
+ * main - main function to generate a random  number
+ *
+ * Return: Always 0 (Success)
  *
  */
 
