faff8264d7321f6ae014130d7c869c9ec3d6b1e2
pitchou95 <pitchoudeve@gmail.com>
2023-06-21 08:04:27 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,11 @@
 
 int print_last_digit(int n)
 {
-    int last;
+	int last;
 
-    last = n % 10;
-    if (last < 0)
-        last = last * -1;
-    _putchar(last + '0');
-    return last;
+	last = n % 10;
+	if (last < 0)
+		last = last * -1;
+	_putchar(last + '0');
+	return (last);
 }
-
