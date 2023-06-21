60f65658c24bb1f338e76654b37d6ec9151ec7bf
pitchou95 <pitchoudeve@gmail.com>
2023-06-21 09:45:24 -0700

    9-times_table

---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * time_table - Prints the 9 times table, starting with 0
+ */
+void time_table(void)
+{
+    /* Your implementation of the time_table function */
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    time_table();
+    return 0;
+}
+
