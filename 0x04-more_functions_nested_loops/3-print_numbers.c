7692ab7d5370d9eb312ad5073a9c73738eb522f1
pitchou95 <pitchoudeve@gmail.com>
2023-06-22 19:10:32 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+ * print_numbers - check for check for a digit (0 through 9).
+ *
+ * Retun: Always 0.
+ */
+void print_number(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		putchar(c);
+	}
+
+	_putchar('\n');
+}
