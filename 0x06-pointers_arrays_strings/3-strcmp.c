cd49d1f1079ee4c9c54c4032f8227f38684f0a0f
pitchou95 <pitchoudeve@gmail.com>
2023-06-30 10:30:38 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,4 @@
-#include "main"
+#include "main.h"
 
 /**
  * _strcmp - Compare two strings
@@ -16,11 +16,11 @@ int _strcmp(char *s1, char *s2)
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
-		{
 			return (s1[i] - s2[i]);
-		}
+
 		i++;
 	}
+
 	return (0);
 }
 
