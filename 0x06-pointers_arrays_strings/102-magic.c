a1753c6ab3ea3b1df73b40a595e23d67d3de7aa5
pitchou95 <pitchoudeve@gmail.com>
2023-06-30 19:59:55 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -7,18 +7,23 @@
  */
 int main(void)
 {
-	int n;
-	int a[5];
-	int *p;
+    int n;
+    int a[5];
+    int *p;
 
-	a[2] = 1024;
-	p = &n;
-
-	*(p + 5) = 98;
-
-	/* Print the value of a[2] */
-	printf("a[2] = %d\n", a[2]);
-
-	return (0);
+    a[2] = 1024;
+    p = &n;
+    /*
+     * Write your line of code here...
+     * Remember:
+     * - You are not allowed to use "a"
+     * - You are not allowed to modify "p"
+     * - Only one statement is allowed
+     * - You are not allowed to code anything else than this line of code
+     */
+    *(p + 5) = 98;
+    /* ...so that this prints 98\n */
+    printf("a[2] = %d\n", a[2]);
+    return (0);
 }
 
